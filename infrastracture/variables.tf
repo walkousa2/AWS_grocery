260239f970c2fe67266f97b7f0a0ef73af4ba0f3
walkousa2 <walkousa@gmail.com>
2026-01-31 12:50:39 +0100

    Add email_address and aws_region variables

---
+++ infrastracture/variables.tf
@@ -0,0 +1,10 @@
+variable "email_address" {
+  description = "Your email adress"
+  type        = string
+  default     = "youradress@gmail.com"
+}
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+  default     = "eu-north-1"
+}
